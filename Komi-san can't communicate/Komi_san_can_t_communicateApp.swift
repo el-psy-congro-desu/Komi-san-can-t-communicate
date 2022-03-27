2b770076a1cdfce911d51ad1cd56d7e6e1bfcbef
Vladimir Lapskiy <v.lapskij@yandex.ru>
2022-03-27 17:50:19 +0500

    Initial Commit

---
+++ Komi-san can't communicate/Komi_san_can_t_communicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Komi_san_can_t_communicateApp.swift
+//  Komi-san can't communicate
+//
+//  Created by Vladimir Lapskiy on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Komi_san_can_t_communicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
